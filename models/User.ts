f3e138ec2bc58a4c3d7556ce260a55fc9556efe3
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-08-15 20:39:41 +0530

    done the getaccesstoken

--- models/User.ts
+++ models/User.ts
@@ -1,28 +1,38 @@
-import mongoose ,{Schema , Document, mongo} from "mongoose"
-import jsonwebtoken from "jsonwebtoken"
+import mongoose, { Schema, Document, mongo } from "mongoose";
+import jwt from "jsonwebtoken";
 
-interface User extends Document{
-    email: string
-    password: string
+interface User extends Document {
+  email: string;
+  password: string;
 }
 
-const UserSchema : Schema<User> = new Schema({
+const UserSchema: Schema<User> = new Schema(
+  {
     email: {
-        type: String,
-        required:[true , "Please provide the eamil id"]
+      type: String,
+      required: [true, "Please provide the eamil id"],
     },
-    password:{
-        type:String,
-        required:[true , "Please provide the password"]
-    }
-},{
-    timestamps: true
-})
+    password: {
+      type: String,
+      required: [true, "Please provide the password"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 ///access token
-UserSchema.methods.generateAccessToken = ()=>{
+UserSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      
+    },
+    process.env.JWT_SECRET as string
+  );
+};
 
-}
-
-const User = mongoose.models.User || mongoose.model<User>("User" , UserSchema)
-export default User
+const User = mongoose.models.User || mongoose.model<User>("User", UserSchema);
+export default User;
