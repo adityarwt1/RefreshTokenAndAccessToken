256ac6c33815f504e28d237af1f07d2b4924dede
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-08-15 20:54:17 +0530

    user model is done

--- models/User.ts
+++ models/User.ts
@@ -1,6 +1,6 @@
 import mongoose, { Schema, Document, mongo } from "mongoose";
 import jwt from "jsonwebtoken";
-
+import crypto from "crypto"
 interface User extends Document {
   email: string;
   password: string;
@@ -30,9 +30,33 @@ UserSchema.methods.generateAccessToken = function () {
       email: this.email,
       
     },
-    process.env.JWT_SECRET as string
+    process.env.JWT_SECRET as string,{
+      expiresIn: "1d"
+    }
   );
 };
+
+/// refresh token 
+UserSchema.methods.generateRefreshToken = function(){
+  return jwt.sign({
+    _id: this._id,
+ },
+  process.env.JWT_SECRET as string,{
+    expiresIn:"10d"
+  })
+  }
+
+
+/// generate temporary
+UserSchema.methods.generateTemporaryToken = function(){
+  const unHanshedToken = crypto.randomBytes(20).toString()
+  const hashedToken = crypto
+          .createHash("sha256")
+          .update(unHanshedToken)
+          .digest("hex")
+  const tokenExpiry = Date.now() + (10*60*1000) // 10 minute
+  return {unHanshedToken, hashedToken, tokenExpiry}
+}
 
 const User = mongoose.models.User || mongoose.model<User>("User", UserSchema);
 export default User;
